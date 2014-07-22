4b20469a27af28216a744c1644eed18c490890ca
xiaoxu yang <liquorbar@163.com>
2014-07-22 19:50:12 +0800

    can html
    复制HTML内容部分

--- js/background.js
+++ js/background.js
@@ -1,3 +1,19 @@
+var debug = (function(){
+    //开启调试功能,可以在上线时关闭
+    DEBUG = true;
+    /* An empty debug function */
+    var _emptyFunc = function(msg) { return false; };
+    var _debugFunc = function(msg) { console.log('[DEBUG] ' + msg); };
+    var debug = _emptyFunc;
+
+    // if DEBUG is true and Firebug console is available, use it for logging
+    if (window.DEBUG && typeof(console) !== 'undefined') {
+        return _debugFunc;
+    }else{
+        return _emptyFunc;
+    }
+})();
+
 chrome.contextMenus.create({
     'type':'normal',
     'title':'设置标题',
@@ -5,7 +21,6 @@ chrome.contextMenus.create({
     'id':'tit',
     'onclick':setTitle
 });
-
 chrome.contextMenus.create({
     'type':'normal',
     'title':'设置简介',
@@ -13,7 +28,6 @@ chrome.contextMenus.create({
     'id':'summary',
     'onclick':setSummary
 });
-
 chrome.contextMenus.create({
     'type':'normal',
     'title':'设置内容',
@@ -22,52 +36,74 @@ chrome.contextMenus.create({
     'onclick':setContent
 });
 
-chrome.contextMenus.create({
-    'type':'normal',
-    'title':'提交',
-    'contexts':['selection'],
-    'id':'submit',
-    'onclick':toSubmit
-});
-
 var title,summary,content;
 
 function setTitle(info, tab){
     title = info.selectionText;
-    console.log(title);
     chrome.contextMenus.update('tit',{
         'title':'标题已设置'
     });
+    showSubmit();
 }
-
 function setSummary(info, tab){
     summary = info.selectionText;
     chrome.contextMenus.update('summary',{
         'title':'简介已设置'
     });
+    showSubmit();
 }
-
 function setContent(info, tab){
-    content = info.selectionText;
-    chrome.contextMenus.update('content',{
-        'title':'内容已设置'
-    });
+    console.log("请使用Shift + c复制HTML内容");
 }
 
-function toSubmit(){
+
+/*
+ * Message passing between content script and backgroud page
+ */
+chrome.extension.onMessage.addListener(
+    function(request) {
+      switch (request.command) {
+        case 'copy':
+            //console.log(request.data);
+            content = request.data;
+            chrome.contextMenus.update('content',{
+                'title':'内容已设置'
+            });
+            showSubmit();
+            break;
+        default:
+            break;
+        }
+    }
+);
+
+var showSubmit = function(){
+    if(title && summary && content){
+        chrome.contextMenus.create({
+            'type':'normal',
+            'title':'提交',
+            'contexts':['page'],
+            'id':'submit',
+            'onclick':toSubmit
+         });
+    }
+}
+
+var resetData = function(){
     //还原title，重新添加内容
     chrome.contextMenus.update('tit',{
         'title':'设置标题'
     });
-
     chrome.contextMenus.update('summary',{
         'title':'设置简介'
     });
-
     chrome.contextMenus.update('content',{
         'title':'设置内容'
     });
+    var title = undefined,summary = undefined,content= undefined;
+}
 
+function toSubmit(){
     if(!title || !summary || !content){
         console.log("缺必选项！");
     }else{
@@ -78,6 +114,7 @@ function toSubmit(){
             timeout: 30000
         }).done(function(msg) {
             console.log(msg);
+            resetData();
         }).fail(function(jqXHR, textStatus) {
             console.log(textStatus);
         });
--- js/content.js
+++ js/content.js
@@ -1,40 +1,32 @@
 (function() {
-	window.onmouseup = function() {
-		var selection = window.getSelection();
-		if (selection.anchorOffset != selection.extentOffset) {
-			chrome.runtime.sendMessage(selection.toString());
-		}
-	}
+    var debug = (function(){
+        //开启调试功能,可以在上线时关闭
+        DEBUG = true;
+        /* An empty debug function */
+        var _emptyFunc = function(msg) { return false; };
+        var _debugFunc = function(msg) { console.log('[DEBUG] ' + msg); };
+        var debug = _emptyFunc;
 
-
-	/* An empty debug function */
-    var _emptyFunc = function(msg) {};
-    var _debugFunc = function(msg) { console.log('[DEBUG] ' + msg); };
-    var debug = _emptyFunc;
-
-    // if DEBUG is true and Firebug console is available, use it for logging
-    if (window.DEBUG && typeof(console) !== 'undefined') {
-        debug = _debugFunc;
-    }else{
-    	debug = _emptyFunc;
-    }
-
+        // if DEBUG is true and Firebug console is available, use it for logging
+        if (window.DEBUG && typeof(console) !== 'undefined') {
+            return _debugFunc;
+        }else{
+            return _emptyFunc;
+        }
+    })();
 
 	/*
      * Get the selected content
      */
-    function getSelectedContent(sel, textOnly)
-    {
+    function getSelectedContent(sel, textOnly) {
         var div = document.createElement('div');
         var range = sel.getRangeAt(0);
 
         if (!textOnly) { // Expand the selection if want to copy html code
-            while(range.startContainer.nodeType == document.TEXT_NODE
-                  || range.startContainer.childNodes.length == 1)
+            while (range.startContainer.nodeType == document.TEXT_NODE || range.startContainer.childNodes.length == 1)
                 range.setStartBefore(range.startContainer);
 
-            while(range.endContainer.nodeType == document.TEXT_NODE
-                  || range.endContainer.childNodes.length == 1)
+            while (range.endContainer.nodeType == document.TEXT_NODE || range.endContainer.childNodes.length == 1)
                 range.setEndAfter(range.endContainer);
         }
 
@@ -57,24 +49,27 @@
      */
     function html(sel) { return getSelectedContent(sel, false); }
 
+    /* 
+     * Get selected text content
+     */
+    function text(sel) { return getSelectedContent(sel, true); }
 
     /*
      * Copy non-empty value to clipboard
      */
     function copy(value, mode)
     {
-        if (value.match(/^(\s|\n)*$/) != null)
-            return;
+        // if (value.match(/^(\s|\n)*$/) != null)
+        //     return;
 
         mode = mode || 'default';
-        debug('Copyied string: ' + value + ', copy mode: ' + mode);
-
         chrome.extension.sendMessage({
             command: 'copy',
             data: value,
             mode: mode
         }, function (response) {
             // do nothing
+            console.log(response);
         });
     }
 
@@ -82,16 +77,26 @@
 	/*
 	 * Mouseup event handler
 	 */
-	function onmouseup(event) {
-		 var sel = event.view.getSelection();
-		 value = html(sel);
-		 // copy(value);
+	function onkeydown(event) {
+		var sel = event.view.getSelection();
+
+        var raw = true;
+        var value = "";
+
+        if (event.shiftKey && event.keyCode == 67) /* Shift + c */
+            raw = true;
+        else if (event.ctrlKey && event.keyCode == 67) /* Ctrl + c */
+            raw = false;
+        else
+            return;
+
+        value = raw ? text(sel) : html(sel);
+        //debug(value);
+		copy(value);
+        sel.removeAllRanges();
 	}
 
 
-
-	document.addEventListener('mouseup', onmouseup, false);
-
-
+    document.addEventListener('keydown', onkeydown, false);
 
 })();
